6d272f1e84814811f631610cb19b457a87c89a28
BlesK <maksarestov2000@gmail.com>
2023-09-20 21:20:01 +0700

    fix supervisord

---
+++ nginx_files/conf/pull.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Переходим в директорию с вашим репозиторием
+cd /home/blesk/nginx-http3/ 
+
+# Получаем последние изменения из репозитория
+git fetch
+
+# Получаем имя текущей ветки
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+# Проверяем, есть ли изменения в текущей ветке
+if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
+    # Если есть изменения, то выполняем команду git pull
+    git pull origin $branch
+    echo "Репозиторий успешно обновлен"
+else
+    echo "Репозиторий уже на последнем обновлении"
+fi
+

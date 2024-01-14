0682bd68ff8442a52e0a2944a460ff1601ec1900
main <main@gmail.com>
2024-01-14 18:08:17 +0000

    com

--- MainApp/models.py
+++ MainApp/models.py
@@ -4,8 +4,6 @@ from django.db import models
 
 class CloudUser(AbstractUser):
     node_domain = models.CharField(max_length=20)
-#    user_access_token = models.CharField(max_length=256, default='user_access_token')
-#    user_refresh_token = models.CharField(max_length=256, default='user_refresh_token')
 #    secret_key = models.CharField(max_length=64, default='secret_key')
 
 
@@ -17,8 +15,6 @@ class NodeModel(models.Model):
     EX_IP = models.CharField(max_length=15, unique=True)
     UUID = models.CharField(max_length=32, unique=True)
     local_connection = models.CharField(max_length=5, default="False")
-#    local_server_access_token = models.CharField(max_length=256, default="local_server_access_token")
-#    local_server_refresh_token = models.CharField(max_length=256, default="local_server_refresh_token")
     node_access_token = models.CharField(max_length=256, default="node_access_token")
     node_refresh_token = models.CharField(max_length=256, default="node_refresh_token")
     secret_key = models.CharField(max_length=64, default="secret_key")

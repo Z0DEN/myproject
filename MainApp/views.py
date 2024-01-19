7157677e806e6284f6eb2518927ed18096430f21
main <main@gmail.com>
2024-01-19 16:37:36 +0000

    user validate

--- MainApp/views.py
+++ MainApp/views.py
@@ -79,30 +79,29 @@ def GetToken(user):
 
 @csrf_exempt
 def TokenVerify(request):
+    response_data = {
+        'node_validate_status': None,
+        'user_validate_status': None,
+    }
+
     data = json.loads(request.body)
     bearer_header = request.headers.get('Authorization')
     node_bearer_token = bearer_header.split(' ')[1]
     node_UUID = data['node_UUID']
     node_secret_key = NodeModel.objects.get(UUID=node_UUID).secret_key
-    _, decode_server_token_status = decode_token(node_bearer_token, node_secret_key)
-    if decode_server_token_status != 22:
-        response_data = {
-            'node_validate_status': decode_server_token_status,
-            'user_validate_status': 10
-        }
-        return RJR(response_data=response_data)
+    _, validate_server_status = decode_token(node_bearer_token, node_secret_key)
+    response_data['node_validate_status'] = validate_server_status
+    if validate_server_status != 22:
+        return RJR(response_data=response_data, status=22)
 
     user_token = data['Bearer']
     username = data['username']
     user_secret_key = REDISKA.get(f'user_secret_key:{username}')
     if user_secret_key is None:
-        return RJR(status=22)
+        return RJR(response_data=response_data, status=22)
 
-    _, decode_user_status = decode_token(user_token, user_secret_key) 
-    response_data = {
-        'node_validate_status': decode_server_token_status,
-        'user_validate_status': decode_user_status,
-    }
+    _, validate_user_status = decode_token(user_token, user_secret_key) 
+    response_data['user_validate_status'] = validate_user_status
     return RJR(response_data=response_data, status=22)
 
 

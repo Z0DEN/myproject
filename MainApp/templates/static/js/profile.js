b7c5b52316f50ce757d6815054ea4bb7b89ca386
main <main@gmail.com>
2024-02-12 16:06:51 +0000

    working upd tokens

--- MainApp/templates/static/js/profile.js
+++ MainApp/templates/static/js/profile.js
@@ -8,26 +8,6 @@ function mainFunc(){
 }
 
 
-function updateTokens(){
- console.log('start updating tokens');
- fetch('https://whoole.space/UserTokenUpdate/', {
-   method: 'POST',
-   headers: {
-     'Content-Type': 'application/json',
-     'X-CSRFToken': csrf_cookie, 
-   },
- })
- .then(response => response.json())
- .then(data => {
-   console.log(data);
-   if (data.status != 23){
-      window.location.href = "/logout/";
-   }
-   return data.status
- })
-}
-
-
 function createFile(){
   let input = document.getElementById("file-input");
   
@@ -53,37 +33,54 @@ function getUserData(){
 }
 
 
-function makeRequest(func='GetUserData', body={}) {
- access_token = Cookies.get('access_token');
- fetch(`https://${node_domain}.whoole.space:8002/${func}/`, {
+async function updateTokens(){
+ console.log('start updating tokens');
+ let response = await fetch('https://whoole.space/UserTokenUpdate/', {
    method: 'POST',
    headers: {
      'Content-Type': 'application/json',
-     'Authorization': `user Bearer ${access_token}`
+     'X-CSRFToken': csrf_cookie,
    },
-   body: JSON.stringify({
-     username: username,
-     ...body,
-   })
- })
- .then(response => response.json())
- .then(data => {
+ });
+ let data = await response.json();
+ console.log(data);
+ if (data.status != 23){
+      window.location.href = "/logout/";
+ }
+ return data.status;
+}
+
+
+async function makeRequest(func='GetUserData', body={}) {
+ try {
+   access_token = Cookies.get('access_token');
+   let response = await fetch(`https://${node_domain}.whoole.space:8002/${func}/`, {
+     method: 'POST',
+     headers: {
+       'Content-Type': 'application/json',
+       'Authorization': `user Bearer ${access_token}`
+     },
+     body: JSON.stringify({
+       username: username,
+       ...body,
+     })
+   });
+   let data = await response.json();
    console.log(data);
-   status = data.status
+   status = data.status;
    if (status == 22){
-     return data
+     return data;
    } else if (status == 14 || status == 15 || status == "null"){
-     upd_tokens_status = updateTokens()
+     upd_tokens_status = await updateTokens();
      if (upd_tokens_status == 23){
-     	return makeRequest(func, body)
+        return makeRequest(func, body);
      }
    } else if (status == 31){
-     return makeRequest(func, body)
+     return makeRequest(func, body);
    }
-   })
- .catch((error) => {
+ } catch (error) {
    console.error('Error:', error);
- });
+ }
 }
 
 

3a66bad3affe1ccf55b23b633a1b0d517869813e
main <main@gmail.com>
2024-03-08 17:25:05 +0000

    com

--- myproject/settings.py
+++ myproject/settings.py
@@ -49,7 +49,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-f790b_syh@as9^%j(e290_k8mgsq=7mgznh^g0eu(231#w+j!l"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 # Application definition
@@ -172,9 +172,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "MainApp/templates/static/"),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "MainApp/templates/static/")
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "MainApp/templates/static/"),
+#]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
---
+++ wsgi.py
@@ -0,0 +1,10 @@
+import os
+from django.core.asgi import get_asgi_application
+from daphne import server
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+
+application = get_wsgi_application()
+
+#server.run(application, port=8003, interface='127.0.0.1') 
+#server.run(application, port=8001, interface='127.0.0.1', ssl_keyfile='/etc/letsencrypt/live/cloudblesk.site/privkey.pem', ssl_certfile='/etc/letsencrypt/live/cloudblesk.site/cert.pem')

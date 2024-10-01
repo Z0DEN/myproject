9b5acd7636a98758c92ffcfff1f51c141e1d049d
blesk <archbtw@blesk.corp>
2024-10-01 16:29:12 +0000

    chng static directory in settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -173,7 +173,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "MainApp/templates/static/")
+# STATIC_ROOT = os.path.join(BASE_DIR, "MainApp/templates/static/")
+STATIC_ROOT = "/main_files/www/static"
 #STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR, "MainApp/templates/static/"),
 #]

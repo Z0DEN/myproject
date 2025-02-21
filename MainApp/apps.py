6d56a5c4a8f3e6453e39cae488f75309cc16de5d
BlesK <109669748+Z0DEN@users.noreply.github.com>
2025-02-22 00:04:46 +0700

    Update apps.py

--- MainApp/apps.py
+++ MainApp/apps.py
@@ -4,3 +4,15 @@ from django.apps import AppConfig
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'MainApp'
+    def ready(self):
+            from MainApp.models import ServerDataModel
+    
+            personal_key = os.environ.get("PERSONAL_KEY")
+    
+            if personal_key:
+                try:
+                    entry = ServerDataModel.objects.get(id=1)
+                    entry.personal_key = personal_key
+                    entry.save()
+                except ObjectDoesNotExist:
+                    ServerDataModel.objects.create(personal_key=personal_key)
